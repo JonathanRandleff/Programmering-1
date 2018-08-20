20b5dd1c0a2d3764c22de196ff4dd27da8516538
JonathanRandleff <39033097+JonathanRandleff@users.noreply.github.com>
2018-08-20 14:11:34 +0200

    langd improved.

--- OU3/DenKortasteVagen.java
+++ OU3/DenKortasteVagen.java
@@ -17,11 +17,11 @@ public class DenKortasteVagen {
                 double xu = a[i];
                 double uv = b[i][j];
                 double vy = c[j];
-                double totalLängd = xu+uv+vy;
-                if (totalLängd < tempo) {
+                double totalLangd = xu+uv+vy;
+                if (totalLangd < tempo) {
                     kortastVag[0] = i+1;
                     kortastVag[1] = j+1;
-                    tempo = totalLängd;
+                    tempo = totalLangd;
                 }
             }
         }
@@ -35,33 +35,22 @@ public class DenKortasteVagen {
      * @param c Stationerna vid sträckan V - Y
      * @return Längden mellan X och Y med den kortaste vägen.
      */
-    public static double langd (double[] a, double[][] b, double[] c)
-    {
-        int n = 12;
-        double [] längder = new double [n];
-        for (int i = 0; i<3; i++)
-        {
-            for (int j = 0; j<4; j++)
-            {
-                int temp =+ j + (4*i);
+    public static double langd (double[] a, double[][] b, double[] c) {
+        double tempo = 1000000;
+        double kortastLangd = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
                 double xu = a[i];
                 double uv = b[i][j];
                 double vy = c[j];
-                double totalLängd = xu+uv+vy;
-                System.out.println ("Längd för X, U" + (i+1) + ", V" + (j+1) + ", Y: " + totalLängd);
-                längder[temp] = totalLängd;
+                double totalLangd = xu + uv + vy;
+                System.out.println ("Längd för X, U" + (i+1) + ", V" + (j+1) + ", Y: " + totalLangd);
+                if (totalLangd < tempo) {
+                    kortastLangd = totalLangd;
+                    tempo = totalLangd;
+                }
             }
         }
-        int forst = 0;
-        int sist = längder.length -1;
-        int minst = forst;
-        int aktuell = forst +1;
-        while (aktuell <= sist)
-        {
-            if (längder[aktuell]<längder[minst])
-                minst = aktuell;
-            aktuell++;
-        }
-        return längder[minst];
+        return kortastLangd;
     }
 }
